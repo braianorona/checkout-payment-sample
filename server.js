78262dbd922c52f41f34b883abc36f83b640ef9b
Braian <braianorona@gmail.com>
2021-10-04 15:09:48 -0300

    cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors({
   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
-  origin: 'https://folcamp-scholas-test.netlify.app/'
+  origin: '*'
 }));
 app.use(express.static("../../client"));
 
