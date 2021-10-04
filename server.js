bd38a9320543b7679fa0acfe99f23f38c7f243ab
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-10-04 18:06:47 -0300

    a
    Co-authored-by: Braian Orona <braianorona@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,69 +1,77 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mercadopago = require("mercadopago");
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mercadopago = require('mercadopago')
+// const axios = require('axios').default()
 
 // REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
 mercadopago.configure({
-	access_token: "TEST-1525632293088814-100414-957f02583329f006389847e732f36d1d-228186099",
-});
-  
-const port = 8085
+  access_token:
+    'TEST-1525632293088814-100414-957f02583329f006389847e732f36d1d-228186099'
+})
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors({
-  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
-  origin: '*'
-}));
-app.use(express.static("../../client"));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(
+  cors({
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+    origin: '*'
+  })
+)
+// app.use(express.static('../../client'))
 
-app.get("/", function (req, res) {
-  res.status(200).sendFile("index.html");
-}); 
+// app.get('/', function (req, res) {
+//   res.status(200).sendFile('index.html')
+// })
 
-app.post("/create_preference", (req, res) => { 
+app.post('/create_preference', (req, res) => {
+  let preference = {
+    items: [
+      {
+        title: req.body.description,
+        unit_price: Number(req.body.price),
+        quantity: Number(req.body.quantity)
+      }
+    ],
+    back_urls: {
+      success: 'https://scholas-node.herokuapp.com/feedback',
+      failure: 'https://scholas-node.herokuapp.com/feedback',
+      pending: 'https://scholas-node.herokuapp.com/feedback'
+    },
+    auto_return: 'approved'
+  }
 
-	let preference = {
-		items: [
-			{
-				title: req.body.description,
-				unit_price: Number(req.body.price),
-				quantity: Number(req.body.quantity),
-			}
-		],
-		back_urls: {
-			"success": "https://scholas-node.herokuapp.com/feedback",
-			"failure": "https://scholas-node.herokuapp.com/feedback",
-			"pending": "https://scholas-node.herokuapp.com/feedback"
-		},
-		auto_return: "approved",
-	};
+  mercadopago.preferences
+    .create(preference)
+    .then((response) => {
+      res.json({
+        id: response.body.id
+      })
+      return response.body.id
+    })
+    .then((preferenceId) => {
+      console.log(preferenceId)
+      //   axios.get('https://api.mercadopago.com/v1/payments/1242010966')
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+})
 
-	mercadopago.preferences.create(preference)
-		.then(function (response) {
-			res.json({
-				id: response.body.id
-			});
-		}).catch(function (error) {
-			console.log(error);
-		});
-});
-
-app.post("/webhook", (req, res) => {
+app.post('/mercadopago/notifications', (req, res) => {
   console.log(req.body)
+
   res.status('200').json(req.body)
 })
 
-
-app.get('/feedback', function(req, res) {
-	res.json({
-		Payment: req.query.payment_id,
-		Status: req.query.status,
-		MerchantOrder: req.query.merchant_order_id
-	});
-});
+app.get('/feedback', function (req, res) {
+  res.json({
+    Payment: req.query.payment_id,
+    Status: req.query.status,
+    MerchantOrder: req.query.merchant_order_id
+  })
+})
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log("The server is now running on Port 5000");
-});
+  console.log('The server is now running on Port 5000')
+})
